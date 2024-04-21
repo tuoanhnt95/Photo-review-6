9ca4a24b5fa8b6aa30672dd1f31c8c68bb598efc
Oanh Nguyen <myspamvn@yahoo.com>
2024-04-21 23:41:30 +0900

    feat: add font-awesome

--- app/javascript/entrypoints/setupEntryPoint.ts
+++ app/javascript/entrypoints/setupEntryPoint.ts
@@ -1,11 +1,22 @@
 import { Component, createApp } from 'vue';
 import { Router } from 'vue-router';
 import { VueQueryPlugin } from 'vue-query';
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+/* import specific icons */
+import { faPlus, faX, faHouse, faBell, faGear, faUser, faPen, faCalendarDays, faFloppyDisk, faCheck, faXmark, faQuestion, faCaretRight, faCaretLeft, faCaretDown, faArrowLeft, faExclamation, faRotateLeft, faGripLines, faTableColumns, faCircleCheck, faShareFromSquare, faEllipsis, faBorderAll, faList, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+import { faTrashCan, faSquareMinus } from '@fortawesome/free-regular-svg-icons'
+
 import { globalProperties } from './globalProperties';
 import { pinia } from '@/stores';
 import { setHTTPHeader } from '@/services/http.service';
 import AuthService from '@/services/auth.service';
 
+/* add icons to the library */
+library.add(faPlus, faX, faHouse, faBell, faGear, faUser, faPen, faCalendarDays, faFloppyDisk, faCheck, faXmark, faQuestion, faCaretRight, faCaretLeft, faCaretDown, faArrowLeft, faExclamation, faRotateLeft, faGripLines, faTableColumns, faCircleCheck, faShareFromSquare, faEllipsis, faBorderAll, faList, faEye, faEyeSlash)
+library.add( faTrashCan, faSquareMinus )
 const token = AuthService.getToken();
 
 if (token) {
@@ -17,8 +28,8 @@ export const setupEntryPoint = (rootComponent: Component, router: Router) => {
 
   app.use(router);
   app.use(pinia);
+  app.component('font-awesome-icon', FontAwesomeIcon);
   app.use(VueQueryPlugin);
-  app.config.globalProperties = { ...app.config.globalProperties, ...globalProperties };
-
+  app.config.globalProperties = { ...app.config.globalProperties, ...globalProperties }
   app.mount('#app');
 };
