a2fbcd8b74fc08141b00a72cd20b49dc00a3e507
Oanh Nguyen <myspamvn@yahoo.com>
2024-07-01 23:36:20 +0900

    feat: sort albums feature

--- app/javascript/pages/panel/AlbumsPage.vue
+++ app/javascript/pages/panel/AlbumsPage.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="flex w-100 justify-center">
+  <div class="flex w-100 justify-center px-4">
     <div>
       <div :class="{ 'opacity-25': action.length > 0 }">
-        <div class="label-text mt-4">Albums</div>
-        <div class="flex w-100">
-          <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mx-auto mb-6 z-0">
-            <div class="w-36 h-44" @click="action = 'create'">
+        <div class="h-11">Settings</div>
+        <div class="label-text">Albums</div>
+        <div class="container-sort">
+          <div class="container-sort-label" @click="showSort">
+            {{ selectedSort }}
+            <font-awesome-icon
+              v-if="!isShowingSort"
+              icon="fa-solid fa-angle-down"
+              class="sort-icon"
+            />
+            <font-awesome-icon v-if="isShowingSort" icon="fa-solid fa-angle-up" class="sort-icon" />
+          </div>
+          <div class="container-sort-menu">
+            <div
+              class="sort-menu bg-glass-dark shadow-lg divide-y divide-solid divide-neutral-700"
+              :class="{ show: isShowingSort }"
+            >
               <div
-                class="flex w-100 h-36 rounded border border-solid border-slate-600 cursor-pointer"
+                v-for="sort in sortMethods"
+                :key="sort.value"
+                class="sort-menu-item text-xs px-4 py-2"
+                :class="{ 'sort-selected pointer-events-none': sortMethod === sort.value }"
+                @click="sortMethod = sort.value"
               >
-                <font-awesome-icon icon="fa-solid fa-plus" class="m-auto text-violet-600" />
+                {{ sort.name }}
               </div>
-              <div class="pt-1 text-xs text-slate-400">New album</div>
             </div>
+          </div>
+        </div>
+        <div class="flex pb-4">
+          <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-6 mx-auto mb-6 z-0">
             <div v-for="album in albums" :key="album.id">
               <div class="relative w-36 h-46 cursor-pointer">
                 <RouterLink :to="{ name: 'Album', params: { id: album.id } }">
@@ -27,10 +47,15 @@
                   <div class="pl-1 text-md truncate font-medium text-slate-500 text-white">
                     {{ album.name }}
                   </div>
-                  <div class="pl-1 text-xs text-slate-400">Expire: {{ album.expiry_date }}</div>
+                  <div class="pl-1 text-xs text-slate-400">Due: {{ album.expiry_date }}</div>
                 </RouterLink>
               </div>
             </div>
+          </div>
+        </div>
+        <div @click="action = 'create'">
+          <div class="btn-circle-add">
+            <font-awesome-icon icon="fa-solid fa-plus" class="m-auto" />
           </div>
         </div>
       </div>
@@ -76,7 +101,23 @@ onBeforeMount(async () => {
 });
 
 const albums = computed(() => {
-  return albumsData.value;
+  const albumsDataCopy = albumsData.value;
+  switch (sortMethod.value) {
+    case 1:
+      return albumsDataCopy.sort(
+        (a, b) => new Date(a.expiry_date).getTime() - new Date(b.expiry_date).getTime(),
+      );
+    case 2:
+      return albumsDataCopy.sort(
+        (a, b) => new Date(b.expiry_date).getTime() - new Date(a.expiry_date).getTime(),
+      );
+    case 3:
+      return albumsDataCopy.sort((a, b) => a.name.localeCompare(b.name));
+    case 4:
+      return albumsDataCopy.sort((a, b) => b.name.localeCompare(a.name));
+    default:
+      return albumsData.value;
+  }
 });
 
 const action = ref('');
@@ -86,4 +127,79 @@ const addAlbum = (album: Album) => {
   album.cover = '';
   albumsData.value.unshift(album);
 };
+
+// sort methods: 0 album added order 1 far to close, 2 close to far, 3 alphabetical ascending, 4 alphabetical descending
+const sortMethods = [
+  { value: 0, name: 'Creation date: Old to New' },
+  { value: 1, name: 'Due date: Earliest First' },
+  { value: 2, name: 'Due date: Latest First' },
+  { value: 3, name: 'Album name: A to Z' },
+  { value: 4, name: 'Album name: Z to A' },
+];
+const sortMethod = ref(0);
+const isShowingSort = ref(false);
+const selectedSort = computed(() => {
+  const selected = sortMethods.find((x) => x.value === sortMethod.value);
+  return selected ? selected.name : sortMethods[0].name;
+});
+const showSort = () => {
+  isShowingSort.value = !isShowingSort.value;
+  console.log('showSort');
+};
 </script>
+
+<style scoped>
+@import '../../assets/css/panel.scss';
+
+.container-sort {
+  position: relative;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 44px;
+}
+
+.container-sort-label {
+  display: flex;
+  gap: 0.5rem;
+  margin-left: 0.25rem;
+  cursor: pointer;
+  font-size: 12px;
+
+  .sort-icon {
+    align-self: center;
+    font-size: 10px;
+  }
+}
+
+.sort-menu {
+  position: absolute;
+  top: 16px;
+  inset-inline-end: -9px;
+  z-index: 10;
+  width: 190px;
+  border-radius: var(--border-radius-lg);
+  color: var(--color-text-light-1);
+  opacity: 0;
+  transition: opacity 0.1s ease-in-out;
+}
+
+.container-sort-menu {
+  position: relative;
+}
+.sort-menu.show {
+  opacity: 1;
+}
+
+.sort-menu-item {
+  cursor: pointer;
+}
+
+.sort-menu-item:hover {
+  color: var(--color-primary);
+}
+
+.sort-selected {
+  color: var(--color-text);
+}
+</style>
