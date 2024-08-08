bd6d2504fd499a0b7fe5a2594915319e37b72fe8
Oanh Nguyen <myspamvn@yahoo.com>
2024-08-09 02:34:45 +0900

    feat: add images

--- app/javascript/pages/website/IndexPage.vue
+++ app/javascript/pages/website/IndexPage.vue
@@ -10,32 +10,61 @@
         </div>
       </div>
     </div>
-    <div class="collab border border-solid mt-36 grid grid-cols-2">
-      <!-- <div class="grid grid-cols-2 gap-2">
-        <img src="/photoshoot.webp" alt="photoshoot" />
-        <img src="/wedding.webp" alt="wedding" />
-        <img src="/product.webp" alt="product" />
-        <img src="/headshot.webp" alt="headshot" />
-      </div> -->
-      <!-- <div class="grid grid-rows-2"> -->
-      <div>
+    <div class="collab grid grid-cols-1 lg:grid-cols-2 mx-16 mt-36">
+      <div class="border border-solid border-r-0 border-violet-500">
         <img src="/teamwork.webp" alt="headshot" class="w-full h-fit" />
       </div>
       <!-- </div> -->
-      <ul class="p-16 pt-24 text-xl flex flex-col gap-4">
-        <li>You had a photoshoot. Now, you need to retouch.</li>
-        <li class="text-2xl">Do you:</li>
-        <li>Select from <strong class="text-2xl">hundreds of photos</strong>?</li>
-        <li>
-          Decide in <strong class="text-2xl">collaboration</strong> with clients, photographers, art
-          director, creative director, etc.?
+      <ul
+        class="p-16 pt-24 text-sm sm:text-md md:text-xl flex flex-col gap-4 border border-solid border-l-0"
+      >
+        <li class="text-base md:text-lg xl:text-xl 2xl:text-2xl">
+          After a photoshoot, do you have to:
         </li>
-        <li>
-          Chain yourself to a <strong class="text-2xl">big work station</strong> all day long?
+        <li class="flex gap-3 items-center">
+          <font-awesome-icon
+            icon="fa-solid fa-check"
+            class="sm:text-lg md:text-xl text-violet-500"
+          />
+          <div>
+            Select from
+            <strong class="md:text-lg xl:text-xl 2xl:text-2xl">hundreds of photos</strong>?
+          </div>
         </li>
-        <li>
-          Prefer <strong class="text-2xl">budget</strong> or
-          <strong class="text-2xl">free</strong> solutions?
+        <li class="flex gap-3 items-center">
+          <font-awesome-icon icon="fa-solid fa-check" class="text-xl text-violet-500" />
+          <div class="w-full">
+            Decide in <strong class="md:text-lg xl:text-xl 2xl:text-2xl">collaboration</strong> with
+            <div class="lg:truncate lg:text-ellipsis lg:overflow-hidden xl:flex xl:items-baseline">
+              <span
+                v-for="(
+                  letter, i
+                ) in 'clients, editors, creative director, art directors, designer, team members'"
+                :key="i"
+                :style="{
+                  fontSize: 1.25 - i * 0.01 + 'rem',
+                }"
+              >
+                {{ letter }}
+              </span>
+              ?
+            </div>
+          </div>
+        </li>
+        <li class="flex gap-3 items-center">
+          <font-awesome-icon icon="fa-solid fa-check" class="text-xl text-violet-500" />
+          <div>
+            Chain yourself to a
+            <strong class="md:text-lg xl:text-xl 2xl:text-2xl">big work station</strong> all day
+            long?
+          </div>
+        </li>
+        <li class="flex gap-3 items-center">
+          <font-awesome-icon icon="fa-solid fa-check" class="text-xl text-violet-500" />
+          <div>
+            <strong class="md:text-lg xl:text-xl 2xl:text-2xl">Pay</strong> for collaborative review
+            tools?
+          </div>
         </li>
       </ul>
     </div>
@@ -54,13 +83,10 @@
         <p class="mt-40 mb-8 text-4xl">Photo culling is <strong>simple</strong>.</p>
         <p class="text-2xl mb-2">All you have to do is select</p>
         <table>
-          <tr v-for="opt in reviewDisplayOptions.reverse()" :key="opt.value">
+          <tr v-for="opt in reviewDisplayOptions" :key="opt.value">
             <td>
-              <div class="flex items-center mb-4">
-                <font-awesome-icon
-                  :icon="`fa-solid fa-${opt.icon}`"
-                  class="flex items-center text-2xl"
-                />
+              <div class="mb-2">
+                <font-awesome-icon :icon="`fa-solid fa-${opt.icon}`" class="text-2xl" />
               </div>
             </td>
             <td class="text-2xl pl-4">
@@ -68,15 +94,79 @@
             </td>
           </tr>
         </table>
-        <div
-          v-for="opt in reviewDisplayOptions.reverse()"
-          :key="opt.value"
-          class="flex gap-4 items-center"
-        >
+        <div v-for="opt in reviewDisplayOptions" :key="opt.value" class="flex gap-4 items-center">
           <p></p>
         </div>
       </div>
     </div>
+
+    <div class="grid md:grid-cols-2 gap-8 px-16 select mt-20">
+      <div class="container-image multiplatform collab relative">
+        <img
+          src="/desktopOffice 2.jpeg"
+          alt="desktop office"
+          class="w-3/5 opacity-50"
+          style="filter: blur(1px)"
+        />
+        <div class="w-3/5 h-full z-20 absolute left-0 top-0">
+          <div class="flex h-full justify-center items-center">
+            <font-awesome-icon
+              icon="fa-solid fa-check"
+              class="sm:text-lg md:text-xl text-violet-500"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="container-image multiplatform collab relative flex justify-end">
+        <img
+          src="/laptopCafe.webp"
+          alt="laptop cafe"
+          class="w-3/5 opacity-50"
+          style="filter: blur(1px)"
+        />
+        <div class="w-3/5 h-full z-20 absolute left-0 top-0">
+          <div class="flex h-full justify-center items-center">
+            <font-awesome-icon
+              icon="fa-solid fa-question"
+              class="sm:text-lg md:text-xl text-violet-500"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="container-image multiplatform collab relative">
+        <img
+          src="/phoneTrain.jpeg"
+          alt="phone train"
+          class="w-3/5 opacity-50"
+          style="filter: blur(1px)"
+        />
+        <div class="w-3/5 h-full z-20 absolute left-0 top-0">
+          <div class="flex h-full justify-center items-center">
+            <font-awesome-icon
+              icon="fa-solid fa-xmark"
+              class="sm:text-lg md:text-xl text-violet-500"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="container-image multiplatform collab relative flex justify-end">
+        <img
+          src="/phoneHome.webp"
+          alt="phone home"
+          class="w-3/5 opacity-80"
+          style="filter: blur(1px)"
+        />
+        <div class="w-3/5 h-full z-20 absolute left-0 top-0">
+          <div class="flex h-full justify-center items-center">
+            <font-awesome-icon
+              icon="fa-solid fa-check"
+              class="sm:text-lg md:text-xl text-violet-500"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+
     <div class="container-features border border-solid mt-20">
       <div class="container-image">
         <img src="/photocrew.webp" alt="portrait" />
@@ -98,9 +188,9 @@
 
 <script setup lang="ts">
 const reviewDisplayOptions = [
+  { value: 'Yes', icon: 'check' },
   { value: 'No', icon: 'xmark' },
   { value: 'Maybe', icon: 'question' },
-  { value: 'Yes', icon: 'check' },
 ];
 </script>
 
