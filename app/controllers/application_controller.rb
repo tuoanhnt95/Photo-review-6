6d3a1bd4708b3207cdb39b4cd4e32026714b6c9e
Oanh Nguyen <myspamvn@yahoo.com>
2024-08-27 20:19:48 +0900

    fix: uncomment redirect after authentication, add nested module

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,15 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   include Pagy::Backend
-  include Devise::Controllers::Helpers
 
   after_action { pagy_headers_merge(@pagy) if @pagy }
-
-  # before_action :authenticate_user!, only: %i[panel]
 
   def website
     render template: 'layouts/website'
   end
 
   def panel
-    p 'env secret key 1'
-    # RAILS.logger.info ENV.fetch('secret_key_base', nil)
-    # RAILS.logger.info ENV.fetch('DEVISE_JWT_SECRET_KEY', nil)
-    # RAILS.logger.info ENV.fetch('devise_jwt_secret_key', nil)
-
-    # p ENV.fetch('secret_key_base', nil)
-    p 'panel'
-    p user_signed_in?
-    # return redirect_to website_path unless user_signed_in?
+    return redirect_to website_path unless user_signed_in?
 
     render template: 'layouts/panel'
   end
--- app/controllers/panel/reviews_controller.rb
+++ app/controllers/panel/reviews_controller.rb
@@ -1,54 +1,56 @@
 # frozen_string_literal: true
 
-class ReviewsController < ApplicationController
-  before_action :set_review, only: %i[show update destroy]
+module Panel
+  class ReviewsController < ApplicationController
+    before_action :set_review, only: %i[show update destroy]
 
-  # GET /reviews
-  def index
-    @reviews = Review.all
+    # GET /reviews
+    def index
+      @reviews = Review.all
 
-    render json: @reviews
-  end
-
-  # GET /reviews/1
-  def show
-    render json: @review
-  end
-
-  # POST /reviews
-  def create
-    @review = Review.new(review_params)
-
-    if @review.save
-      render json: @review, status: :created, location: @review
-    else
-      render json: @review.errors, status: :unprocessable_entity
+      render json: @reviews
     end
-  end
 
-  # PATCH/PUT /reviews/1
-  def update
-    if @review.update(review_params)
+    # GET /reviews/1
+    def show
       render json: @review
-    else
-      render json: @review.errors, status: :unprocessable_entity
     end
-  end
 
-  # DELETE /reviews/1
-  def destroy
-    @review.destroy
-  end
+    # POST /reviews
+    def create
+      @review = Review.new(review_params)
 
-  private
+      if @review.save
+        render json: @review, status: :created, location: @review
+      else
+        render json: @review.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_review
-    @review = Review.find(params[:id])
-  end
+    # PATCH/PUT /reviews/1
+    def update
+      if @review.update(review_params)
+        render json: @review
+      else
+        render json: @review.errors, status: :unprocessable_entity
+      end
+    end
 
-  # Only allow a list of trusted parameters through.
-  def review_params
-    params.require(:review).permit(:name)
+    # DELETE /reviews/1
+    def destroy
+      @review.destroy
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_review
+      @review = Review.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def review_params
+      params.require(:review).permit(:name)
+    end
   end
 end
