ce8776b3fa8b3b51d6c8131bc0439b2f8fd6ca92
Oanh Nguyen <myspamvn@yahoo.com>
2024-08-27 19:57:54 +0900

    fix: remove unnecessary setups

--- app/javascript/apis/auth.api.ts
+++ app/javascript/apis/auth.api.ts
@@ -3,13 +3,13 @@ import { http } from '@/services/http.service';
 import { IRegisterUser, IUserLogin } from '@/types/general';
 
 export const login = (user: IUserLogin): Promise<AxiosResponse> => {
-  return http.post('users/login', { user });
+  return http.post('/users/login', { user });
 };
 
 export const logout = (): Promise<AxiosResponse> => {
-  return http.delete('users/logout');
+  return http.delete('/users/logout');
 };
 
 export const register = (user: IRegisterUser): Promise<AxiosResponse> => {
-  return http.post('users/signup', { user });
+  return http.post('/users/signup', { user });
 };
